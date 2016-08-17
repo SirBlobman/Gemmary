835e9a910fa86969746ade7315d69ae306ba9e37
SirBlobman <blobmansrealm@gmail.com>
2016-08-16 22:44:53 -0400

    Beta 0.0.2.4
    Added more to the tooltip (ALT key = NBT)
    Changed some Tooltip entries to have Bold

--- 1.10.2/src/java/com/SirBlobman/gemmary/item/GemmaryTooltip.java
+++ 1.10.2/src/java/com/SirBlobman/gemmary/item/GemmaryTooltip.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import com.SirBlobman.gemmary.GUtil;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class GemmaryTooltip 
+public class GemmaryTooltip
 {
 	@SubscribeEvent
 	public void tooltip(ItemTooltipEvent e)
@@ -21,23 +23,39 @@ public class GemmaryTooltip
 		if(is == null || i == null) return;
 		if(GuiScreen.isShiftKeyDown())
 		{
-			toolTip.add("§9Gemmary:");
+			toolTip.add("§9§lGemmary:");
 			if(GUtil.getMohsScaleNumber(i) > 0.0D) toolTip.add("  " + GUtil.getMohsValueOfItem(i));
-			toolTip.add("  ID (unused): " + Item.getIdFromItem(i));
-			toolTip.add("  Unlocalized Name: " + i.getUnlocalizedName());
-			toolTip.add("  Registry Name: " + i.getRegistryName());
-			toolTip.add("  Class: " + i.getClass().getName());
-			toolTip.add("  Mod: " + i.getRegistryName().getResourceDomain());
-			toolTip.add("  Durability: " + is.getItemDamage() + "/" + is.getMaxDamage());
+			toolTip.add("  §lID (unused): §r" + Item.getIdFromItem(i));
+			toolTip.add("  §lMeta: §r" + is.getMetadata());
+			toolTip.add("  §lUnlocalized Name: §r" + i.getUnlocalizedName());
+			toolTip.add("  §lRegistry Name: §r" + i.getRegistryName());
+			if(i instanceof ItemBlock)
+			{
+				ItemBlock ib = (ItemBlock) i;
+				Block b = ib.getBlock();
+				toolTip.add("  §lBlock Class: §r" + b.getClass().getName());
+			}
+			toolTip.add("  §lClass: §r" + i.getClass().getName());
+			toolTip.add("  §lMod: §r" + i.getRegistryName().getResourceDomain());
+			int meta = is.getItemDamage();
+			int max = is.getMaxDamage();
+			int damage = max - meta; 
+			if(max != 0) toolTip.add("  §lDurability: §r" + damage + "/" + max);
 			if(GUtil.getOreDictionaryEntries(i).size() != 0) 
 			{
-				toolTip.add("  Ore Dictionary:");
+				toolTip.add("  §lOre Dictionary:");
 				for(String s : GUtil.getOreDictionaryEntries(i)) toolTip.add("    - " + s);
 			}
+			
 		}
 		else
 		{
 			toolTip.add("§7§oPress SHIFT to see more information...");
 		}
+		if(GuiScreen.isAltKeyDown()) 
+		{
+			toolTip.add(is.serializeNBT().toString());
+		}
+		else toolTip.add("Press ALT to see NBT Data...");
 	}
 }
